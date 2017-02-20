8c25a3b7a7ebeb60624703ca801ed94adb4e6749
harshana <jmhmjayamaha@gmail.com>
2017-02-20 22:42:37 +0530

    initial commit

---
+++ src/main/java/lk/harshana/Application.java
@@ -0,0 +1,13 @@
+package lk.harshana;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/lk/harshana/model/Person.java
@@ -0,0 +1,6 @@
+package lk.harshana.model;
+
+
+public class Person {
+
+}
