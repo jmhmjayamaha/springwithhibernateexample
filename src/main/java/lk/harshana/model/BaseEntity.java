621f19cd69a87a4b34ab3a8547795b342cf172cc
harshana <jmhmjayamaha@gmail.com>
2017-02-23 22:57:34 +0530

    add comments

--- src/main/java/lk/harshana/model/BaseEntity.java
+++ src/main/java/lk/harshana/model/BaseEntity.java
@@ -1,9 +1,18 @@
+/**
+ * 
+ *  @author Harshana Madusanka Jayamaha
+ */
+
 package lk.harshana.model;
 
 import java.io.Serializable;
 
+/**
+ * The Class BaseEntity.
+ */
 public class BaseEntity implements Serializable {
 
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 	
 }
--- src/main/java/lk/harshana/service/impl/PersonServiceImpl.java
+++ src/main/java/lk/harshana/service/impl/PersonServiceImpl.java
@@ -14,34 +14,53 @@ import lk.harshana.dao.def.PersonDAO;
 import lk.harshana.model.Person;
 import lk.harshana.service.def.PersonService;
 
+/**
+ * The Class PersonServiceImpl.
+ */
 @Service
 public class PersonServiceImpl implements PersonService{
 
+	/** The person dao. */
 	@Autowired
 	private PersonDAO personDao;
 	
+	/* (non-Javadoc)
+	 * @see lk.harshana.service.def.PersonService#addPerson(lk.harshana.model.Person)
+	 */
 	public boolean addPerson(final Person person) {
 		personDao.addPerson(person);
 		return false;
 	}
 
+	/* (non-Javadoc)
+	 * @see lk.harshana.service.def.PersonService#getAllPersons()
+	 */
 	public List<Person> getAllPersons() {
 		
 		return personDao.getAllPersons();
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see lk.harshana.service.def.PersonService#getPerson(int)
+	 */
 	public Person getPerson(final int pid) {
 		
 		return personDao.getPerson(pid);
 	}
 
+	/* (non-Javadoc)
+	 * @see lk.harshana.service.def.PersonService#updatePerson(lk.harshana.model.Person)
+	 */
 	public void updatePerson(final Person person) {
 		
 		personDao.updatePerson(person);
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see lk.harshana.service.def.PersonService#deletePerson(int)
+	 */
 	public void deletePerson(final int pid) {
 		
 		personDao.deletePerson(pid);
